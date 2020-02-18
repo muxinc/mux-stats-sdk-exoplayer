c9826d0c2fbbf850b06e36c878a5c696139ee9dc
Tomislav Kordic <tkordic.private@gmail.com>
2020-02-18 11:03:55 +0100

    added intermecco tests.

--- MuxExoPlayer/src/androidTest/java/com/mux/stats/sdk/muxstats/MuxStatsLateInitInstrumentationTest.java
+++ MuxExoPlayer/src/androidTest/java/com/mux/stats/sdk/muxstats/MuxStatsLateInitInstrumentationTest.java
@@ -1,12 +1,19 @@
 package com.mux.stats.sdk.muxstats;
 
+import android.app.Activity;
 import android.content.Context;
 import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.runner.AndroidJUnit4;
+
+import com.mux.stats.sdk.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -20,6 +27,9 @@ public class MuxStatsLateInitInstrumentationTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+
+        ViewInteraction interaction = onView(withId(R.id.player_view));
+        interaction.perform(click());
 
         assertEquals("com.example.hotmicsyncproto", appContext.getPackageName());
     }
