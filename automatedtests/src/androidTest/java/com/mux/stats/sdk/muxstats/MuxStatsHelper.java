f205b1ac38f2fe4ca8abdeb6526344316656ff5b
Tomislav Kordic <tkordic.private@gmail.com>
2021-06-01 15:24:20 -0300

    code reformat.

--- automatedtests/src/androidTest/java/com/mux/stats/sdk/muxstats/MuxStatsHelper.java
+++ automatedtests/src/androidTest/java/com/mux/stats/sdk/muxstats/MuxStatsHelper.java
@@ -1,6 +1,7 @@
 package com.mux.stats.sdk.muxstats;
 
 public class MuxStatsHelper {
+
   public static void allowHeaderToBeSentToBackend(MuxStatsExoPlayer muxStats, String headerName) {
     muxStats.allowHeaderToBeSentToBackend(headerName);
   }
