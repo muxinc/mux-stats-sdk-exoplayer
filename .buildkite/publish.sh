d3efc1adc8e467df1ccf5c18699cec21f960eaf5
nbirkenshawmux <nbirkenshaw@mux.com>
2021-08-09 14:00:33 -0400

    Running the publish via a shell script from buildkite

---
+++ .buildkite/publish.sh
@@ -0,0 +1,14 @@
+# This script is called by the buildkite pipeline
+#
+# Accessing the secret containing env vars in here prevents buildkite from capturing them
+
+docker run -it -v --rm  \
+    -v $(pwd):/data \
+    -e ORG_GRADLE_PROJECT_signingKeyId="$ORG_GRADLE_PROJECT_signingKeyId" \
+    -e ORG_GRADLE_PROJECT_signingPassword="$ORG_GRADLE_PROJECT_signingPassword" \
+    -e ORG_GRADLE_PROJECT_signingKey="$ORG_GRADLE_PROJECT_signingKey" \
+    -e ORG_GRADLE_PROJECT_artifactory_user=$ORG_GRADLE_PROJECT_artifactory_user \
+    -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
+    -w /data \
+    muxinc/mux-exoplayer:20201215 \
+    bash -c "./gradlew --info MuxExoPlayer:clean MuxExoPlayer:build assemble automatedtests:assembleAndroidTest artifactoryPublish"
