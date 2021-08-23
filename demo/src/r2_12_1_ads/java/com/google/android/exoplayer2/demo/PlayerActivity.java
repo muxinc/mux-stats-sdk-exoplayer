5817e2704d953ccc7e0cfd80456a74e97b933bb9
Tomislav Kordic <32546640+tomkordic@users.noreply.github.com>
2021-08-23 20:58:47 +0200

    For API version 30 and greater we are now using context.getDisplay instead of WindowManager. (#145)

--- demo/src/r2_12_1_ads/java/com/google/android/exoplayer2/demo/PlayerActivity.java
+++ demo/src/r2_12_1_ads/java/com/google/android/exoplayer2/demo/PlayerActivity.java
@@ -22,6 +22,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Point;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.KeyEvent;
@@ -349,6 +350,11 @@ public class PlayerActivity extends AppCompatActivity
       muxStats = new MuxStatsExoPlayer(
           this, player, "demo-player", customerData);
       Point size = new Point();
+      if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+        getWindowManager().getDefaultDisplay().getSize(size);
+      } else {
+        getApplication().getApplicationContext().getDisplay().getSize(size);
+      }
       getWindowManager().getDefaultDisplay().getSize(size);
       muxStats.setScreenSize(size.x, size.y);
       muxStats.setPlayerView(playerView);
