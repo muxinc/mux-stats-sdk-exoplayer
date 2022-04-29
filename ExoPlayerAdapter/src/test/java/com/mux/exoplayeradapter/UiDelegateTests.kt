f084d5fe6acfd3b5e8f13c7523c5b170f0b7ddce
Emily Dixon <edixon@mux.com>
2022-04-29 16:36:28 -0700

    Start Tests again

---
+++ ExoPlayerAdapter/src/test/java/com/mux/exoplayeradapter/UiDelegateTests.kt
@@ -0,0 +1,14 @@
+package com.mux.exoplayeradapter
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UiDelegateTests {
+
+  @Test
+  fun addition_isCorrect() {
+    assertEquals(4, 2 + 2)
+  }
+
+  fun mock
+}
