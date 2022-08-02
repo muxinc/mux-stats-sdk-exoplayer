cf39c2a303e501ecdc46ca8d0da18f03086b7962
Emily Dixon <edixon@mux.com>
2022-08-02 15:07:45 -0700

    Add null checks for 2.18.1 as well

--- MuxExoPlayer/src/r2_18_1/java/com/mux/stats/sdk/muxstats/MuxStatsExoPlayer.java
+++ MuxExoPlayer/src/r2_18_1/java/com/mux/stats/sdk/muxstats/MuxStatsExoPlayer.java
@@ -293,6 +293,9 @@ public class MuxStatsExoPlayer extends MuxBaseExoPlayer implements AnalyticsList
 
   @Override
   public void onPlaybackStateChanged(EventTime eventTime, @Player.State int state) {
+    if (player == null || player.get() == null) {
+      return;
+    }
     onPlaybackStateChanged(state);
   }
 
@@ -309,6 +312,10 @@ public class MuxStatsExoPlayer extends MuxBaseExoPlayer implements AnalyticsList
   @Override
   public void onPlayWhenReadyChanged(AnalyticsListener.EventTime eventTime, boolean playWhenReady,
       int reason) {
+    if (player == null || player.get() == null) {
+      return;
+    }
+
     onPlayWhenReadyChanged(playWhenReady, reason);
     onPlaybackStateChanged(player.get().getPlaybackState());
   }
@@ -379,6 +386,10 @@ public class MuxStatsExoPlayer extends MuxBaseExoPlayer implements AnalyticsList
 
   @Override
   public void onPlaybackStateChanged(int playbackState) {
+    if (player == null || player.get() == null) {
+      return;
+    }
+
     /*
      * Sometimes onPlaybackStateChanged callback will not be triggered, and it is
      * prone to bugs to keep same value in two places, so we should always access
@@ -534,6 +545,9 @@ public class MuxStatsExoPlayer extends MuxBaseExoPlayer implements AnalyticsList
 
   @Override
   public void onTimelineChanged(Timeline timeline, int reason) {
+    if (player == null || player.get() == null) {
+      return;
+    }
     handleHlsManifest(player.get());
 
     if (timeline != null && timeline.getWindowCount() > 0) {
