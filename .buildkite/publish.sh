fb9ec1269bf4c0e0a619207999beb6d700fb202c
Emily Dixon <edixon@mux.com>
2022-09-08 17:03:52 -0700

    Revert "Hey maybe this will save a ton of time"
    This reverts commit aa20c09706677fccce8c04e622d1396619f99d83.

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -15,3 +15,15 @@ docker run -it --rm  \
     -w /data \
     docker.io/muxinc/mux-exoplayer:20220112 \
     bash -c "./gradlew --info MuxExoPlayer:clean MuxExoPlayer:assemble MuxExoPlayer:artifactoryPublish"
+
+docker run -it --rm  \
+    -v $(pwd):/data \
+    -e BUILDKITE_BRANCH="$BUILDKITE_BRANCH" \
+    -e ORG_GRADLE_PROJECT_signingKeyId="$ORG_GRADLE_PROJECT_signingKeyId" \
+    -e ORG_GRADLE_PROJECT_signingPassword="$ORG_GRADLE_PROJECT_signingPassword" \
+    -e ORG_GRADLE_PROJECT_signingKey="$ORG_GRADLE_PROJECT_signingKey" \
+    -e ORG_GRADLE_PROJECT_artifactory_user=$ORG_GRADLE_PROJECT_artifactory_user \
+    -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
+    -w /data \
+    docker.io/muxinc/mux-exoplayer:20220112 \
+    bash -c "./gradlew --info automatedtests:assemble automatedtests:assembleAndroidTest"
