60ec7b7a5cdba8b7d378df56d7e635eeee8d879d
Emily Dixon <edixon@mux.com>
2022-09-10 06:34:12 -0700

    Add a Release Config for ExoPlayerAdapter + Cleanup Build Scripts + Faster CI (#230)

--- ExoPlayerAdapter/src/test/java/com/mux/exoplayeradapter/AbsRobolectricTest.kt
+++ ExoPlayerAdapter/src/test/java/com/mux/exoplayeradapter/AbsRobolectricTest.kt
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
-@Config(manifest = Config.NONE)
+@Config(
+  manifest = Config.NONE
+)
 @RunWith(RobolectricTestRunner::class)
 abstract class AbsRobolectricTest
