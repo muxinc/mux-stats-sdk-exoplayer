d9344e04d86e2c7babce042bc6156ebc468c8b47
Emily Dixon <edixon@mux.com>
2022-09-19 10:57:16 -0700

    Add GitHub Actions for Basic CI and Release Automation (#235)

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -14,16 +14,4 @@ docker run -it --rm  \
     -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
     -w /data \
     docker.io/muxinc/mux-exoplayer:20220112 \
-    bash -c "./gradlew --stacktrace ExoPlayerAdapter:assemble ExoPlayerAdapter:artifactoryPublish && ./gradlew --stacktrace MuxExoPlayer:assemble MuxExoPlayer:artifactoryPublish"
-
-docker run -it --rm  \
-    -v $(pwd):/data \
-    -e BUILDKITE_BRANCH="$BUILDKITE_BRANCH" \
-    -e ORG_GRADLE_PROJECT_signingKeyId="$ORG_GRADLE_PROJECT_signingKeyId" \
-    -e ORG_GRADLE_PROJECT_signingPassword="$ORG_GRADLE_PROJECT_signingPassword" \
-    -e ORG_GRADLE_PROJECT_signingKey="$ORG_GRADLE_PROJECT_signingKey" \
-    -e ORG_GRADLE_PROJECT_artifactory_user=$ORG_GRADLE_PROJECT_artifactory_user \
-    -e ORG_GRADLE_PROJECT_artifactory_password=$ORG_GRADLE_PROJECT_artifactory_password \
-    -w /data \
-    docker.io/muxinc/mux-exoplayer:20220112 \
     bash -c "./gradlew --stacktrace automatedtests:assembleDebug automatedtests:assembleAndroidTest"
