69e75c8e975cf8affc1bf312e66bc8095d5a186f
Emily Dixon <edixon@mux.com>
2022-11-08 12:52:56 -0800

    Update to MuxCore 7.4.0

--- MuxExoPlayer/src/main/java/com/mux/stats/sdk/muxstats/MuxStatsExoPlayer.kt
+++ MuxExoPlayer/src/main/java/com/mux/stats/sdk/muxstats/MuxStatsExoPlayer.kt
@@ -642,6 +642,17 @@ private class MuxDevice(ctx: Context) : IDevice {
     return SystemClock.elapsedRealtime()
   }
 
+  override fun outputLog(logPriority: LogPriority, tag: String, msg: String, t: Throwable?) {
+    when (logPriority) {
+      LogPriority.ERROR -> Log.e(tag, msg, t)
+      LogPriority.WARN -> Log.w(tag, msg, t)
+      LogPriority.INFO -> Log.i(tag, msg, t)
+      LogPriority.DEBUG -> Log.d(tag, msg, t)
+      LogPriority.VERBOSE -> Log.v(tag, msg, t)
+      else -> Log.v(tag, msg, t)
+    }
+  }
+
   override fun outputLog(logPriority: LogPriority, tag: String, msg: String) {
     when (logPriority) {
       LogPriority.ERROR -> Log.e(tag, msg)
