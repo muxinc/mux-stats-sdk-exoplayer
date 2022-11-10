af422ea2087bb7e678f9d3f03fb59f15497ea40d
Emily Dixon <edixon@mux.com>
2022-11-10 15:50:16 -0800

    Fix ArrayIndexOutOfBounds Exception after clearing the media item (#253)

--- ExoPlayerAdapter/src/exo-analytics-listener-2_18-now/java/com/mux/stats/sdk/muxstats/internal/ExoAnalyticsListener.kt
+++ ExoPlayerAdapter/src/exo-analytics-listener-2_18-now/java/com/mux/stats/sdk/muxstats/internal/ExoAnalyticsListener.kt
@@ -81,7 +81,7 @@ private class ExoAnalyticsListener(player: ExoPlayer, val collector: MuxStateCol
 
   override fun onTimelineChanged(eventTime: EventTime, reason: Int) {
     val player = player // strong reference during the listener call
-    if (player != null) {
+    if (player != null && eventTime.timeline.windowCount > 0) {
       Timeline.Window().apply {
         eventTime.timeline.getWindow(0, this)
         collector.sourceDurationMs = durationMs
