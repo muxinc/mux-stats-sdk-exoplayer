9923f8eef2b6a79cacf5b174093bbf9ee23a256b
Emily Dixon <edixon@mux.com>
2022-12-19 12:32:27 -0800

    Revert "Detect source url if possible"
    This reverts commit 7c7d5656ae32caf8bd013b3cc4bc9ac522e89318.

--- ExoPlayerAdapter/src/exo-media-item-2_13-2_15/java/com/mux/stats/sdk/muxstats/internal/MediaItem.kt
+++
@@ -1,8 +0,0 @@
-package com.mux.stats.sdk.muxstats.internal
-
-import com.google.android.exoplayer2.MediaItem
-
-@JvmSynthetic
-internal fun MediaItem.getMediaUrl(): String? {
-  return playbackProperties?.uri?.toString()
-}
