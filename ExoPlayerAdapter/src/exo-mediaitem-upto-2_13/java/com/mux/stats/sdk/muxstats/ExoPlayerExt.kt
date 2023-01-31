6ca385ac425dd880856cd3268789dfb91ad13f17
Emily Dixon <edixon@mux.com>
2023-01-31 14:42:43 -0800

    Releases/v3.3.0 (#265)
    ## New
    
    * Add ad-related metadata to ad events (#264)
    
    ## Improvements
    
    * Update to Gradle 7.3 + Wrapper 7.4.0 + Simplify Demo Variant Names (#263)

---
+++ ExoPlayerAdapter/src/exo-mediaitem-upto-2_13/java/com/mux/stats/sdk/muxstats/ExoPlayerExt.kt
@@ -0,0 +1,13 @@
+package com.mux.stats.sdk.muxstats
+
+import com.google.android.exoplayer2.ExoPlayer
+
+/*
+ * # ExoPlayerExt.kt: Useful extensions on [ExoPlayer].
+ */
+
+/**
+ * This method is not supported for this version of ExoPlayer, and it always returns null
+ */
+@Suppress("unused")
+fun ExoPlayer.getAdTagUrl(): String? = null
